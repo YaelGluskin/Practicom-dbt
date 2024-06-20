b065b75cf9ea6f4c65ee67a22ebb154da6b5789d
Eti Gotliv <etigotliv2134@gmail.com>
2024-06-20 15:45:49 +0300

    Update database.sql
    Update VARCHAR user_password to 100

--- backend/database.sql
+++ backend/database.sql
@@ -3,6 +3,6 @@ CREATE DATABASE usersoflogin;
 CREATE TABLE loguser(
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(15)UNIQUE NOT NULL,
-  user_password VARCHAR(15) NOT NULL,
+  user_password VARCHAR(100) NOT NULL,
   email VARCHAR(50) UNIQUE NOT NULL
 );
