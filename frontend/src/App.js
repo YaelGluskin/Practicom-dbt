be9f0a1721ecf1977c575c8de97c69bb24f38078
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-06-20 19:57:37 +0300

    Add Routes
    install 'react-router-dom'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Public from './components/Public';
 import LoginForm from './components/LoginForm';
+import NewUser from './components/NewUser';
+import Home from './components/Home';
 
 function App() {
+  document.title = 'DBT';
   return (
-    <div className="App">
-      <LoginForm/>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route exact path="/" element = {<Layout/>}/> */}
+        {/* 2 public routes */}
+        <Route index element={<Public />} />
+        <Route path="login" element={<LoginForm />} />
+
+        {/* protected routes */}
+        <Route path="newuser" element={<NewUser />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
