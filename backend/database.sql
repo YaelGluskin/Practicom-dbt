63cf536739bf005de4a9d8607b7a06da62e432f9
AvitalShaul <avital4890@gmail.com>
2024-06-20 16:16:25 +0300

    add user_role

--- backend/database.sql
+++ backend/database.sql
@@ -4,5 +4,6 @@ CREATE TABLE loguser(
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(15)UNIQUE NOT NULL,
   user_password VARCHAR(100) NOT NULL,
-  email VARCHAR(50) UNIQUE NOT NULL
+  email VARCHAR(50) UNIQUE NOT NULL,
+  user_role VARCHAR(10) DEFAULT 'USER';
 );
