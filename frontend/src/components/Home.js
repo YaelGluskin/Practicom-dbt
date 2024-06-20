b7ded6685ac12f8197aa1d1278219205c039a070
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-06-20 21:09:27 +0300

    Update Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+function Home() {
+    return (
+        <div>
+            home page
+        </div>
+    );
+}
+
+export default Home;
