665434c70ec7ea93504de6cfe64a3f68db9ac71e
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-07-01 13:21:20 +0300

    Update Home.js
    Creating 5 sessions for presenting articles

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,33 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import {Typography, Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ArticlesList from '../features/ArticleList';
 
-function Home() {
-    return (
-        <div>
-            home page
-        </div>
-    );
-}
+const Home = () => {
+  const navigate = useNavigate();
+  const options = ['option1', 'option2', 'option3', 'option4', 'option5']; // Replace with your actual options
+
+  const handleNewArticle = () => {
+    navigate('/home/newArticle');
+  };
+
+  return (
+    <Box sx={{ padding: 4 }}>
+      <Typography variant="h3" gutterBottom>
+        Plan a Trip
+      </Typography>
+      <Button type="button" variant="contained" color="primary" onClick={handleNewArticle}>
+        New Article
+      </Button>
+      {options.map((option, index) => (
+        <Box key={index} sx={{ marginBottom: 4 }}>
+          <Typography variant="h5" gutterBottom>
+            {option}
+          </Typography>
+          <ArticlesList option={option} />
+        </Box>
+      ))}
+    </Box>
+  );
+};
 
 export default Home;
