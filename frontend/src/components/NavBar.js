afad15b0f3a74e1271669d27b13fba9e7261fcf9
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-07-01 14:01:52 +0300

    Create NavBar.js
    Create Nav Bar for the public area

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,76 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton, Container } from '@mui/material';
+
+
+const NavBar = () => {
+
+    const navigate = useNavigate();
+
+    // Handlers for navigation
+    //const onHomeClicked = () => navigate('/Home');
+    const onContactClicked = () => navigate('/Contact');
+    const onAboutClicked = () => navigate('/About');
+    const onDiscoverClicked = () => navigate('/Discover');
+    // const onLogoutClicked = () => sendLogout();
+    const onLogoutClicked = () => navigate('/LogOut');
+
+
+
+
+    const contactButton = (
+        <IconButton style={{ color: 'white' }} title="Contact" onClick={onContactClicked}>
+            Contact
+        </IconButton>
+    );
+
+
+    const aboutButton = (
+        <IconButton style={{ color: 'white' }} title="About" onClick={onAboutClicked}>
+            About
+        </IconButton>
+    );
+
+    const discoverButton = (
+        <IconButton style={{ color: 'white' }} title="Discover" onClick={onDiscoverClicked}>
+            Discover
+        </IconButton>
+    );
+
+
+    const logoutButton = (
+        <IconButton style={{ color: 'white' }} title="Logout" onClick={onLogoutClicked}>
+            {/* <FontAwesomeIcon icon={faRightFromBracket} /> */}
+        </IconButton>
+    );
+
+
+    const buttonContent = (
+        <>
+            {contactButton}
+            {aboutButton}
+            {discoverButton}
+            {logoutButton}
+        </>
+    );
+
+
+    return (
+        <>
+
+            <AppBar position="static" sx={{ bgcolor: 'primary.main' }}>
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            <Link to="/Home" style={{ textDecoration: 'none', color: 'inherit' }}>DBT</Link>
+                        </Typography>
+                        <nav>
+                            {buttonContent}
+                        </nav>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    );
+};
+
+export default NavBar;
