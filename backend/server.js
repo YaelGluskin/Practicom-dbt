e4ad5772b7db5d60c1c9d14b2ea4c1ea443faaf4
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-07-04 17:29:48 +0300

    feat: Add article routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 //const pool = require("./db");
+//const articleRouter = require('./routes/articleRoutes');
 
 //middleware
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.json()); //req.body
 
 //ROUTES//
 app.use('/loguser', require('./routes/loginRoutes'))
+app.use('/article', require('./routes/articleRoutes'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
