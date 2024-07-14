4c6096ce7c3d46b3c73beaf2380597d5a96c9473
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-07-14 12:08:47 +0300

    feat: Add UserContext and UserProvider components
    This commit adds the UserContext and UserProvider components to the project. The UserContext component is used for managing user data, while the UserProvider component serves as the provider for the UserContext. This addition enhances the application by providing a centralized way to manage and share user data across components.

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,11 +1,12 @@
 // Import necessary dependencies
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { TextField, Button, Container, Grid, Typography, IconButton, InputAdornment } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../Hooks/UserContext';   // Import the UserContext
 
 const LoginForm = () => {
   const { handleSubmit, control, formState: { errors } } = useForm();
@@ -13,18 +14,18 @@ const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false); // Password visibility state
   const navigate = useNavigate() // Hook to navigate to different routes
 
+  const { setUser } = useContext(UserContext);  // Get the setUser function from the UserContext
   const onSubmit = async (data) => {
-    console.log(data);
+    // console.log(data);
 
     if (isLoginMode) {
-      // Send login request
-      //const { username } = data;
       try {
         const response = await axios.post('http://localhost:5001/loguser/login', data);
-        console.log(response.data);
+        // console.log(response.data);
         if (response.data.message === 'Login successful') {
           // Handle successful login
           console.log('User logged in:', response.data.user);
+          setUser(data.username); // Set the user in the context
           navigate(`/home`);
         }
       } catch (error) {
@@ -40,6 +41,7 @@ const LoginForm = () => {
         // Send register
         const response = await axios.post('http://localhost:5001/loguser', data)
         console.log(response)
+        setUser(data.username); // Set the user in the context
         navigate(`/welcome/${data.username}`);
       } catch (error) {// You can handle form submission logic here, e.g., API calls for login or registration
         console.error(error)
