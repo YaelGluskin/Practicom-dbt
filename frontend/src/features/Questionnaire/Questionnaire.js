8d240368b7714fbdad6e04af420980f09fdcb75c
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-08-08 17:16:50 +0300

    feat: Add the other feachers of the Questionnaire

--- frontend/src/features/Questionnaire/Questionnaire.js
+++ frontend/src/features/Questionnaire/Questionnaire.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
 import {
-    Box,
-    Checkbox,
-    FormControlLabel,
-    TextField,
-    Button,
-    Typography,
-    Grid
+    Box, RadioGroup, Radio, Checkbox, FormControlLabel,TextField,
+    Button, FormControl, FormLabel, Typography, Grid, IconButton
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'; // Import the DatePicker component
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'; // Import the AdapterDayjs component
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'; // Import the LocalizationProvider component
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import dayjs from 'dayjs'; // Import the dayjs library
+import CloseIcon from '@mui/icons-material/Close';
 
 const preferences = [ // Define the preferences array
     "Limit Live Event",
@@ -27,31 +20,30 @@ const preferences = [ // Define the preferences array
     "Caves",
     "Cliffs",
     "Mountains",
-    "Waterfalls",
-    "Islands",
-    "Forest",
-    "Entertainment Parks",
-    "Wildlife Attractions",
-    "Museums And Art Galleries",
-    "Unique Built Attractions",
-    "Sport Attractions",
-    "Participating Sport Attractions",
-    "Stadium Tours",
-    "Special Events",
-    "Markets",
-    "Festivals And Parades",
+    // "Waterfalls",
+    // "Islands",
+    // "Forest",
+    // "Entertainment Parks",
+    // "Wildlife Attractions",
+    // "Museums And Art Galleries",
+    // "Unique Built Attractions",
+    // "Sport Attractions",
+    // "Participating Sport Attractions",
+    // "Stadium Tours",
+    // "Special Events",
+    // "Markets",
+    // "Festivals And Parades",
     "Exhibitions"
 ];
 
 const Questionnaire = () => {
     const [selectedPreferences, setSelectedPreferences] = useState([]);
-    // const [keywords, setKeywords] = useState([]);
-    // const [keywordInput, setKeywordInput] = useState(''); 
+    const [keywords, setKeywords] = useState([]);
+    const [keywordInput, setKeywordInput] = useState('');
+
     const navigate = useNavigate(); // Add the useNavigate hook to navigate to the new article form
-    const [startDate, setStartDate] = useState(dayjs()); // Add the startDate state variable
-    const [endDate, setEndDate] = useState(dayjs()); // Add the endDate state variable
-    const formattedStartDate = startDate ? startDate.toLocaleDateString : '';
-    const formattedEndDate = endDate ? endDate.toLocaleDateString : '';
+    const [startDate, setStartDate] = useState(null); // Add the startDate state variable
+    const [endDate, setEndDate] = useState(null); // Add the endDate state variable
 
     const handleCheckboxChange = (event) => { // Define the handleCheckboxChange function
         const { value } = event.target;
@@ -62,41 +54,91 @@ const Questionnaire = () => {
         );
     };
 
-    function Label({ componentName, valueType, isProOnly }) {
-        const content = (
-            <span>
-                <strong>{componentName}</strong> for {valueType} editing
-            </span>
-        );
-    }
-
-    // const handleKeywordChange = (event) => {
-    //     setKeywordInput(event.target.value);
-    // };
-
-    // const handleAddKeyword = () => { 
-    //     if (keywords.length < 10 && keywordInput) { // Add the keyword to the keywords array
-    //         setKeywords((prev) => [...prev, keywordInput]); // Add the keyword to the keywords array
-    //         setKeywordInput(''); // Clear the keyword input field
-    //     }
-    // };
-
-    const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission
-        const formData = { // Create the form data object
-            selectedPreferences, // Add the selected preferences
-            // keywords // Add the keywords
-        };
-        console.log(formData); // Log the form data
-        navigate('/home'); // Navigate to the home page
+    const handleKeywordChange = (event) => { // Define the handleKeywordChange function
+        setKeywordInput(event.target.value);
     };
 
+    const handleAddKeyword = () => {
+        if (keywords.length < 10 && keywordInput) { // Add the keyword to the keywords array
+            setKeywords((prev) => [...prev, keywordInput]); // Add the keyword to the keywords array
+            setKeywordInput(''); // Clear the keyword input field
+        }
+    };
+    const handleDelete = (index) => { // Handle the deletion of a keyword
+        const newKeywords = keywords.filter((_, i) => i !== index);
+        setKeywords(newKeywords);
+    };
+
+
+
+
+
+    const [selectedPurpose, setSelectedPurpose] = useState(''); // Purpose state variable and setPurpose function
+    const handlePurposeChange = (event) => { // Handle the purpose change
+        setSelectedPurpose(event.target.value);
+    };
+    const purposes = [ // Purposes array
+        'Entertainment',
+        'Tourism',
+        'Entertainment and Tourism',
+        'Shopping',
+        'Education',
+        'Business',
+        'Health',
+        'Visiting Family',
+    ];
+    const [selectEdendurance, setSelectEdendurance] = useState(''); // SelectEdendurance state variable and setSelectEdendurance function
+    const handleEnduranceChange = (event) => { // Handle the endurance change
+        setSelectEdendurance(event.target.value);
+    };
+    const endurance = [ // Define the endurance array
+        'A - professional',
+        'B - advanced',
+        'C - family friendly',
+        'D - easy',
+        'E - accessible',
+    ];
+    const [budget, setBudget] = useState();  // Add the budget state variable
+
+    const handleSubmit = (event) => { // Define the handleSubmit function
+        event.preventDefault(); // Prevent the default form submission
+        if (selectedPurpose && selectedPreferences.length > 0 && keywords.length > 0 && startDate && endDate &&
+            selectEdendurance &&  budget
+         ) { // Check if all fields are filled in
+            if (budget < 1000) {
+                alert('Budget must be at least 1000');
+                return;
+            }
+          const formData = { // Create the form data object
+            selectedPurpose, // Add the selected purpose
+            selectedPreferences, // Add the selected preferences
+            keywords, // Add the keywords
+            startDate,
+            endDate,
+            selectEdendurance,
+            budget
+          };
+          console.log(formData); // Log the form data
+          navigate('/home'); // Navigate to the home page
+        } else {
+          alert('Please fill in all fields before submitting the form.');
+        }
+      };
     return (
         <Box sx={{ p: 2 }}>
             <Typography variant="h4" gutterBottom>
                 Preferences Questionnaire
             </Typography>
             <form onSubmit={handleSubmit}>
+                <TextField 
+                    label="Budget" 
+                    type='number' 
+                    value={budget} 
+                    onChange={(event) => setBudget(event.target.value)} 
+                    fullWidth 
+                    margin="normal" 
+                />
+                <Typography variant="subtitle1">You may choose 3 labels:</Typography>
                 <Grid container spacing={2}>
                     {preferences.map((preference, index) => (
                         <Grid item xs={12} sm={6} key={index}>
@@ -105,15 +147,16 @@ const Questionnaire = () => {
                                     <Checkbox
                                         value={preference}
                                         onChange={handleCheckboxChange}
+                                        disabled={!selectedPreferences.includes(preference) && selectedPreferences.length >= 3}
                                     />
                                 }
                                 label={preference}
                             />
                         </Grid>
                     ))}
-                    {/* {<Grid item xs={12}>
+                    <Grid item xs={12}>
                         <TextField
-                            label="Add Keyword"
+                            label="Add Destination: city, country"
                             value={keywordInput}
                             onChange={handleKeywordChange}
                             fullWidth
@@ -123,86 +166,91 @@ const Questionnaire = () => {
                             variant="contained"
                             color="primary"
                             onClick={handleAddKeyword}
-                            disabled={keywords.length >= 10}
+                            disabled={keywords.length >= 1}
                         >
-                            Add Keyword
+                            Add destination
                         </Button>
                         {keywords.length > 0 && (
                             <Box sx={{ mt: 2 }}>
-                                <Typography variant="subtitle1">Keywords:</Typography>
+                                <Typography variant="subtitle1">Destination:</Typography>
                                 <ul>
                                     {keywords.map((keyword, index) => (
-                                        <li key={index}>{keyword}</li>
+                                        <li key={index}>
+                                            {keyword}
+                                            <IconButton
+                                                aria-label="delete"
+                                                size="small"
+                                                onClick={() => handleDelete(index)}
+                                            >
+                                                <CloseIcon fontSize="small" />
+                                            </IconButton>
+                                        </li>
                                     ))}
                                 </ul>
                             </Box>
                         )}
-                    </Grid>} */}
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer
-                            components={[
-                                'DatePicker',
-                                // 'TimePicker',
-                                // 'DateTimePicker',
-                                'DateRangePicker',
-                                // 'DateTimeRangePicker',
-                            ]}
-                        >
-                            <DemoItem label={<Label componentName="DatePicker" valueType="date" />}>
-                                <DatePicker inputFormat="MM/dd/yyyy" value={startDate} onChange={(newValue) => setStartDate(newValue)} />
-                                <DatePicker inputFormat="MM/dd/yyyy" value={endDate} onChange={(newValue) => setEndDate(newValue)} />
-                                {/* <DateRangePicker
-                                    localeText={{
-                                        start: '',
-                                        end: '',
-                                    }}
-                                /> */}
-
-                            </DemoItem>
-                            {endDate && startDate && (TextDecoderStream = endDate.diff(startDate, 'days'))
-                            &&
-                            `Your Trip from ${startDate.toLocaleDateString}
-                            till ${endDate.toLocaleDateString} will last for ${TextDecoderStream} days`}
-
-
-                            {endDate && startDate && (
-                                <Typography variant="subtitle1">
-                                    Your Trip from ${formattedStartDate} till ${formattedEndDate} will last for {TextDecoderStream} days
-                                </Typography>
-                            )}
-                            
-
-                            {/* <DemoItem label={<Label componentName="TimePicker" valueType="time" />}>
-                                <TimePicker />
-                            </DemoItem> */}
-
-                            {/* <DemoItem label={<Label componentName="DatePicker" valueType="date" />}
-                            >
-                                <DateTimePicker />
-                            </DemoItem> */}
-
-
-
-                            {/* <DemoItem
-                                label={
-                                    <Label
-                                        componentName="DateRangePicker"
-                                        valueType="date range"
-                                        isProOnly
+                    </Grid>
+                    <Grid item xs={12}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <DatePicker
+                                        label="Start Date"
+                                        inputFormat="DD/MM/YYYY"
+                                        value={startDate}
+                                        onChange={(newValue) => setStartDate(newValue)}
+                                        minDate={dayjs()}
+                                        renderInput={(params) => <TextField {...params} fullWidth />}
                                     />
-                                }
-                                component="DateRangePicker"
-                            >
-                                <DateRangePicker
-                                    localeText={{
-                                        start: '',
-                                        end: '',
-                                    }}
-                                />
-                            </DemoItem> */}
-
-                        </DemoContainer>
-                    </LocalizationProvider>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <DatePicker
+                                        label="End Date"
+                                        inputFormat="DD/MM/YYYY"
+                                        value={endDate}
+                                        onChange={(newValue) => setEndDate(newValue)}
+                                        minDate={dayjs()}
+                                        renderInput={(params) => <TextField {...params} fullWidth />}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </LocalizationProvider>
+                        {endDate && startDate && (
+                            <Typography variant="body1" sx={{ mt: 2 }}>
+                                Your trip from {startDate.format('DD/MM/YYYY')} till {endDate.format('DD/MM/YYYY')} will last for {endDate.diff(startDate, 'days')} days.
+                            </Typography>
+                        )}
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Purpose of Trip:</FormLabel>
+                            <RadioGroup value={selectedPurpose} onChange={handlePurposeChange}>
+                                {purposes.map((purpose, index) => (
+                                    <FormControlLabel
+                                        key={index}
+                                        value={purpose}
+                                        control={<Radio />}
+                                        label={purpose}
+                                    />
+                                ))}
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Endurance</FormLabel>
+                            <RadioGroup value={selectEdendurance} onChange={handleEnduranceChange}>
+                                {endurance.map((endurance, index) => (
+                                    <FormControlLabel
+                                        key={index}
+                                        value={endurance}
+                                        control={<Radio />}
+                                        label={endurance}
+                                    />
+                                ))}
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
                     <Grid item xs={12}>
                         <Button
                             variant="contained"
